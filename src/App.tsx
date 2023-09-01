f7cbd6f65c9d88b27020cc4cf3a5478a9fcab187
imskanand <the.anand3099@gmail.com>
2023-09-01 23:52:47 +0530

    :truck: : Updated the application to the monorepo - React-ts setup with tailwind-css - Supabase will be added when needed

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="flex justify-center h-screen items-center text-2xl text-green-700 animate-pulse">Welcome to FusionSync</h1>
+    </>
+  )
+}
+
+export default App
