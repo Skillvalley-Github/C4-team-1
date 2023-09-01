cf436dba8ebe1e68cab007c450ecf42ed64e9a46
imskanand <the.anand3099@gmail.com>
2023-09-02 00:54:19 +0530

    :zap: - Added tailwind class shortener :sparkles: - Added prettier for code format

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 const App = () => {
   return (
     <>
-      <h1 className="flex justify-center h-screen items-center text-2xl text-green-700 animate-pulse">Welcome to FusionSync</h1>
+      <h1 className="flex items-center justify-center h-screen text-2xl text-green-700 animate-pulse">
+        Welcome to FusionSync
+      </h1>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
