b08f3923a3ca25ad52eb886c3776f89a5e071e46
CodeKhushal <khushalsachdeva22@gmail.com>
2023-09-12 14:26:17 +0530

    some changes

--- src/components/Shared/Layout.jsx
+++ src/components/Shared/Layout.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router";
-import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
+import Navbar2 from "./Navbar2";
+import Sidebar2 from "./Sidebar2";
 
 const Layout = () => {
   return (
-    <div>
-      <Navbar  />
-      <div>{<Outlet />}</div>
-      <Sidebar />
-
+      // <div>{<Outlet />}</div>
+      <div className="drawer-mobile drawer">
+        <input id="left-sidebar-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col ">
+         <Navbar2 />
+        </div>
+        <Sidebar2 />
     </div>
   );
 };
