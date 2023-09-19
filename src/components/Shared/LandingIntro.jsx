e60876cf05c381febafbf83bb6b250fb287151df
CodeKhushal <khushalsachdeva22@gmail.com>
2023-09-19 18:17:35 +0530

    Minor Corrections

--- src/components/Shared/LandingIntro.jsx
+++ src/components/Shared/LandingIntro.jsx
@@ -1,27 +1,25 @@
-import Intro from "./Intro"
+import Intro from "./Intro";
+import logo from "../../assets/images/logo-red.svg";
+import introImg from "../../assets/images/intro.png";
 
-
-
-function LandingIntro(){
-
-    return(
-        <div className="hero min-h-full rounded-l-xl bg-base-200">
-            <div className="hero-content py-12">
-              <div className="max-w-md">
-
-              <div className="flex justify-center items-center"><img src="src\assets\images\logo-red.svg" className="w-4/5" alt="fusionsync-logo" /></div>
-
-                <div className="text-center my-12"><img src="src\assets\images\intro.png" alt="Dashwind Admin Template" className="w-48 inline-block"></img></div>
-              
-              {/* Importing pointers component */}
-              <Intro />
-              
-              </div>
-
-            </div>
+function LandingIntro() {
+  return (
+    <div className="hero min-h-full rounded-l-xl bg-base-200">
+      <div className="hero-content py-12">
+        <div className="max-w-md">
+          <div className="flex items-center justify-center">
+            <img src={logo} className="w-4/5" alt="fusionsync-logo" />
           </div>
-    )
-      
-  }
-  
-  export default LandingIntro
+
+          <div className="my-12 text-center">
+            <img src={introImg} alt="Dashwind Admin Template" className="inline-block w-48"></img>
+          </div>
+
+          <Intro />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingIntro;
