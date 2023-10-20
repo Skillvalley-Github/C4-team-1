1fac5021bb6248bfca1b0815a59c3574df696853
bharatgl <goyalbharat9977@gmail.com>
2023-10-21 01:45:32 +0530

    UI Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import {useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FusionSync from "./app/Welcome/FusionSync";
 import Login from "./app/Protected/Login";
@@ -26,59 +25,48 @@ import Payslips from "./app/Profile/ProfileSubPages/Payslips";
 import Forms from "./app/Profile/ProfileSubPages/Forms";
 import ProfileSettings from "./app/Profile/ProfileSubPages/ProfileSettings";
 import Overview from "./app/Profile/ProfileSubPages/Overview";
-import ForgotPassword from "./app/Protected/ForgotPassword";
 
-function App() {
-  const [user, setUser] = useState(null)
+const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<FusionSync />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/app/404" element={<InternalPage />} />
-        <Route path="/forgotpassword" element={<ForgotPassword />} />
-        {/* {user ? ( */}
-          <Route path="/dashboard" element={<Layout />}>
-            <Route index path="/dashboard" element={<Dashboard />} />
-            <Route path="/dashboard/attendance" element={<Attendance />} />
-            {/* ---------------Profile Section---------------------------- */}
-            <Route path="/dashboard/profile" element={<ProfileSection />}>
-              <Route index path="/dashboard/profile" element={<Overview />} />
-              <Route path="/dashboard/profile/salarydetails" element={<Salary />} />
-              <Route path="/dashboard/profile/payslips" element={<Payslips />} />
-              <Route path="/dashboard/profile/forms" element={<Forms />} />
-              <Route path="/dashboard/profile/settings" element={<ProfileSettings />} />
-            </Route>
-            {/* ---------------Payroll different tax deductions routes--------------------------------- */}
-            <Route path="/dashboard/payroll/taxdeductions" element={<TaxDeductions />} />
-            <Route path="/dashboard/payroll/taxdeductions/regime-selection" element={<RegimeSelection />} />
-            <Route path="/dashboard/payroll/taxdeductions/home-rent" element={<HomeRentSelection />} />
-            <Route
-              path="/dashboard/payroll/taxdeductions/section-80-deductions"
-              element={<Section80Deductions />}
-            />
-            <Route path="/dashboard/payroll/taxdeductions/home-loan" element={<HomeLoan />} />
-            <Route
-              path="/dashboard/payroll/taxdeductions/leave-travel-allowance"
-              element={<LeaveTravelAllowance />}
-            />
-            <Route path="/dashboard/payroll/taxdeductions/pan-details" element={<PanDetails />} />
-            {/* --------------------tax deductions routes ends here---------------------------- */}
-            <Route path="/dashboard/payroll/reimbursements" element={<Reimbursements />} />
-            <Route path="/dashboard/payroll/reimbursements" element={<Reimbursements />} />
-            <Route path="/dashboard/payroll/payslip" element={<PaySlipGenerator />} />
-            {/* ---------------tasks------------ */}
-            <Route path="/dashboard/tasks" element={<Tasks />} />
-            <Route path="/dashboard/tasks/faq" element={<TaskFaq />} />
-            {/* -------------tasks ends here------------- */}
-            <Route path="/dashboard/leavetracker" element={<LeaveTracker />} />
-            <Route path="/dashboard/employeedata" element={<EmployeeData />} />
+        <Route path="/dashboard" element={<Layout />}>
+          <Route index path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/attendance" element={<Attendance />} />
+          {/* ---------------Profile Section---------------------------- */}
+          <Route path="/dashboard/profile" element={<ProfileSection />} >
+          <Route index path="/dashboard/profile" element={<Overview />} />
+          <Route path="/dashboard/profile/salarydetails" element={<Salary />} />
+          <Route path="/dashboard/profile/payslips" element={<Payslips />} />
+          <Route path="/dashboard/profile/forms" element={<Forms />} />
+          <Route path="/dashboard/profile/settings" element={<ProfileSettings />} />
           </Route>
-        {/* ) : ""} */}
+          {/* ---------------Payroll different tax deductions routes--------------------------------- */}
+          <Route path="/dashboard/payroll/taxdeductions" element={<TaxDeductions />} />
+          <Route path="/dashboard/payroll/taxdeductions/regime-selection" element={<RegimeSelection />} />
+          <Route path="/dashboard/payroll/taxdeductions/home-rent" element={<HomeRentSelection />} />
+          <Route path="/dashboard/payroll/taxdeductions/section-80-deductions" element={<Section80Deductions />} />
+          <Route path="/dashboard/payroll/taxdeductions/home-loan" element={<HomeLoan />} />
+          <Route path="/dashboard/payroll/taxdeductions/leave-travel-allowance" element={<LeaveTravelAllowance />} />
+          <Route path="/dashboard/payroll/taxdeductions/pan-details" element={<PanDetails />} />
+          {/* --------------------tax deductions routes ends here---------------------------- */}
+          <Route path="/dashboard/payroll/reimbursements" element={<Reimbursements />} />
+          <Route path="/dashboard/payroll/reimbursements" element={<Reimbursements />} />
+          <Route path="/dashboard/payroll/payslip" element={<PaySlipGenerator />} />
+          {/* ---------------tasks------------ */}
+          <Route path="/dashboard/tasks" element={<Tasks />} />
+          <Route path = "/dashboard/tasks/faq" element={<TaskFaq/>}/>
+          {/* -------------tasks ends here------------- */}
+          <Route path="/dashboard/leavetracker" element={<LeaveTracker />} />
+          <Route path="/dashboard/employeedata" element={<EmployeeData />} />
+        </Route>
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
