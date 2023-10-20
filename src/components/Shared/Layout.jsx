2161fec781f4e6350f729bfb32e096534210c3c2
bharatgl <goyalbharat9977@gmail.com>
2023-10-21 02:33:20 +0530

    color change

--- src/components/Shared/Layout.jsx
+++ src/components/Shared/Layout.jsx
@@ -6,12 +6,8 @@ import Sidebar from "./Sidebar";
 const Layout = () => {
   return (
     <>
-      <div className="drawer-mobile drawer h-fit overscroll-none bg-red-200 bg-gradient-to-b from-red-200 via-red-300 to-red-400 lg:drawer-open">
-        <input
-          id="left-sidebar-drawer"
-          type="checkbox"
-          className="drawer-toggle"
-        />
+      <div className="drawer-mobile drawer h-fit overscroll-none  bg-gradient-to-r from-sky-200 via-indigo-100 to-indigo-200 lg:drawer-open">
+        <input id="left-sidebar-drawer" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex min-w-full flex-col">
           <Navbar pageTitle={"Dashboard"} />
           <div className="">
