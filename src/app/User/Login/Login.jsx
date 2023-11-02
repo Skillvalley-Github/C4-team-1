fcd67bb564361601c81ebc4ce818051ba877d073
CodeKhushal <khushalsachdeva22@gmail.com>
2023-11-02 20:14:55 +0530

    Removed data leak on console

--- src/app/User/Login/Login.jsx
+++ src/app/User/Login/Login.jsx
@@ -27,7 +27,7 @@ function Login({ setUser }) {
     password: "",
   });
 
-  console.log(loginObj);
+  // console.log(loginObj);
 
   function handleChange(event) {
     setLoginObj((prevLoginObj) => {
@@ -48,7 +48,7 @@ function Login({ setUser }) {
         password: loginObj.password,
       });
       if (error) throw error;
-      console.log(data);
+      // console.log(data);
       setToken(data);
       // setUser(token)
       setLoading(false);
@@ -61,61 +61,63 @@ function Login({ setUser }) {
 
   return (
     <>
-      {loading ? <Spinner />:
-      <div className="flex h-screen items-center bg-base-200">
-        <div className="flex w-full flex-wrap">
-          <div className="flex w-full flex-col md:w-1/2">
-            <div className="my-auto flex flex-col justify-center px-8 pt-8 md:justify-start md:px-24 md:pt-0 lg:px-32">
-              <a href="#" className="mx-auto justify-center">
-                <img src={logo} alt="" />
-              </a>
-              <form className="flex flex-col pb-12 pt-3 md:pt-8" onSubmit={handleSubmit}>
-                <div className="flex flex-col pt-4">
-                  <label htmlFor="email" className="text-lg">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    onChange={handleChange}
-                    placeholder="your@email.com"
-                    className="focus:shadow-outline mt-1 w-full appearance-none rounded border px-3 py-2 leading-tight shadow focus:outline-none"
-                  />
-                </div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="flex h-screen items-center bg-base-200">
+          <div className="flex w-full flex-wrap">
+            <div className="flex w-full flex-col md:w-1/2">
+              <div className="my-auto flex flex-col justify-center px-8 pt-8 md:justify-start md:px-24 md:pt-0 lg:px-32">
+                <a href="#" className="mx-auto justify-center">
+                  <img src={logo} alt="" />
+                </a>
+                <form className="flex flex-col pb-12 pt-3 md:pt-8" onSubmit={handleSubmit}>
+                  <div className="flex flex-col pt-4">
+                    <label htmlFor="email" className="text-lg">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      onChange={handleChange}
+                      placeholder="your@email.com"
+                      className="focus:shadow-outline mt-1 w-full appearance-none rounded border px-3 py-2 leading-tight shadow focus:outline-none"
+                    />
+                  </div>
 
-                <div className="flex flex-col pt-4">
-                  <label htmlFor="password" className="text-lg">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    onChange={handleChange}
-                    placeholder="Password"
-                    className="focus:shadow-outline mt-1 w-full appearance-none rounded border px-3 py-2 leading-tight shadow focus:outline-none"
-                  />
-                </div>
+                  <div className="flex flex-col pt-4">
+                    <label htmlFor="password" className="text-lg">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      onChange={handleChange}
+                      placeholder="Password"
+                      className="focus:shadow-outline mt-1 w-full appearance-none rounded border px-3 py-2 leading-tight shadow focus:outline-none"
+                    />
+                  </div>
 
-                <input
-                  type="submit"
-                  value="Log In"
-                  className="mt-8 bg-black p-2 text-lg font-bold text-white hover:bg-gray-700"
-                />
-              </form>
+                  <input
+                    type="submit"
+                    value="Log In"
+                    className="mt-8 bg-black p-2 text-lg font-bold text-white hover:bg-gray-700"
+                  />
+                </form>
+              </div>
+            </div>
+
+            <div className="w-1/2 shadow-2xl">
+              <img
+                className="hidden h-screen w-full object-cover md:block"
+                src="https://source.unsplash.com/random/?office,employees"
+              />
             </div>
           </div>
-
-          <div className="w-1/2 shadow-2xl">
-            <img
-              className="hidden h-screen w-full object-cover md:block"
-              src="https://source.unsplash.com/random/?office,employees"
-            />
-          </div>
         </div>
-      </div>
-}
+      )}
     </>
   );
 }
